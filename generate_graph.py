d2503b17684a5798a3e41c190d00ceda07da67db
zengkuang <kzeng@connect.ust.hk>
2020-09-05 22:08:35 +0800

    Animation

--- generate_graph.py
+++ generate_graph.py
@@ -17,7 +17,7 @@ nodelist = pd.read_csv('random_nodes.csv')
 buildingnode = pd.read_csv('building_nodes.csv')
 
 source_building = 'A'
-destination_building = 'B'
+destination_building = 'G'
 
 
 g = nx.Graph()
